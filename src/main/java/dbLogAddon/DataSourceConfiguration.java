b669cb045ea1f12ea6039e726b9e041fccf2a23e
Nikita_Alemaskin <nikita_alemaskin@epam.com>
2019-05-21 20:07:03 +0300

    Change to apache connection pool

--- src/main/java/dbLogAddon/DataSourceConfiguration.java
+++ src/main/java/dbLogAddon/DataSourceConfiguration.java
@@ -1,48 +1,34 @@
 package dbLogAddon;
 
-import com.mysql.cj.jdbc.MysqlDataSource;
 import lombok.extern.log4j.Log4j;
+import org.apache.commons.dbcp.BasicDataSource;
 
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 
 
 @Log4j(topic = "file")
 public class DataSourceConfiguration {
 
-    private static final MysqlDataSource MYSQL_DATA_SOURCE = new MysqlDataSource();
-    private static final List<Connection> CONNECTIONS = new ArrayList<>();
+    private static final BasicDataSource MYSQL_DATA_SOURCE = new BasicDataSource();
 
     static {
         Map<String, String> envVar = System.getenv();
-        MYSQL_DATA_SOURCE.setURL(envVar.get("url"));
-        MYSQL_DATA_SOURCE.setUser(envVar.get("username"));
+        MYSQL_DATA_SOURCE.setUrl(envVar.get("url"));
+        MYSQL_DATA_SOURCE.setUsername(envVar.get("username"));
         MYSQL_DATA_SOURCE.setPassword(envVar.get("password"));
+        MYSQL_DATA_SOURCE.setMinIdle(2);
+        MYSQL_DATA_SOURCE.setMaxIdle(5);
+        MYSQL_DATA_SOURCE.setMaxOpenPreparedStatements(10);
     }
 
     private DataSourceConfiguration() {
 
     }
 
-    private static void makeConnectionPool() {
-        if (CONNECTIONS.size() > 2) return;
-        while (CONNECTIONS.size() <= 5) {
-            try {
-                CONNECTIONS.add(MYSQL_DATA_SOURCE.getConnection());
-            } catch (SQLException e) {
-                log.error(e.getMessage(), e);
-            }
-        }
-    }
-
-    public static Connection getConnection() {
-        makeConnectionPool();
-        Connection connection = CONNECTIONS.get(0);
-        CONNECTIONS.remove(0);
-        return connection;
+    public static Connection getConnection() throws SQLException {
+        return MYSQL_DATA_SOURCE.getConnection();
     }
 }
 
